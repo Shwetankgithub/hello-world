f335006bcb7c80f925134093abf357f51339268b
Shwetank Sharma <Shwetank.Sharma@ibm.com>
2022-03-02 10:29:17 +0530

    Test class updated

--- src/com/sk/Test.java
+++ src/com/sk/Test.java
@@ -2,6 +2,6 @@ package com.sk;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test Class main Method");
+        System.out.println("Test Class main Method updated");
     }
 }
